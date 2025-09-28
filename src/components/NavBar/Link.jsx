66c51c9e0445ae37c136a2d32a644de0f08da31b
mst <mst.naher.bd@gmail.com>
2025-09-28 18:29:26 +0100

    added  lucide icon library for React application

---
+++ src/components/NavBar/Link.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Link = ({route}) => {
+  return (
+    <div>
+      <ul className="flex">
+        {
+          <li className="mr-10">
+            <a href={route.path}>{route.name}</a>
+          </li>
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default Link;
